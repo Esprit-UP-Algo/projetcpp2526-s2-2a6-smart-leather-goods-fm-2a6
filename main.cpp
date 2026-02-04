72bb34f86bba9b703ae4027aca6cc5035bb32f35
mersnimedaziz-prog <mersnimedaziz@gmail.com>
2026-02-04 20:27:32 +0100

    V1: Ajout du module (Planification)

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ int main(int argc, char *argv[])
     QTranslator translator;
     const QStringList uiLanguages = QLocale::system().uiLanguages();
     for (const QString &locale : uiLanguages) {
-        const QString baseName = "SmartLeatherFactory_" + QLocale(locale).name();
+        const QString baseName = "ProjetCpp_" + QLocale(locale).name();
         if (translator.load(":/i18n/" + baseName)) {
             a.installTranslator(&translator);
             break;
